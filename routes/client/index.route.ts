358f5c4f2944c553b880d18bb563ff5256b8d40f
Huste6 <quannghihai1@gmail.com>
2024-08-12 21:42:02 +0700

    Danh mục

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,8 +1,10 @@
 import { Express } from "express";
 import { tourRoutes } from "./tour.route";
+import { categoriesRoutes } from "./category.route";
 
 const clientRoutes = (app: Express):void => {
     app.use('/tours',tourRoutes);
+    app.use('/categories',categoriesRoutes);
 }
 
 export default clientRoutes;
