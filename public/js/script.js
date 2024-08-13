e838fd90fe83a736ae3a006716062d4a4a1fdca7
Huste6 <quannghihai1@gmail.com>
2024-08-13 09:42:18 +0700

    Thêm tour vào giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -17,3 +17,34 @@ var imagesMain = new Swiper(".imagesMain", {
     },
 });
 // end slider tour detail
+
+//cart
+// b1 : khoi tao
+const cart = localStorage.getItem("cart");
+if(!cart){
+    localStorage.setItem("cart",JSON.stringify([]));
+}
+// b2 : them tour vao cart
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if(formAddToCart){
+    formAddToCart.addEventListener("submit", (e)=>{
+        e.preventDefault();
+        const quantity = parseInt(e.target.elements.quantity.value);
+        const TourId = parseInt(formAddToCart.getAttribute("tour-id"));
+
+        if(quantity > 0 && TourId){
+            const cart = JSON.parse(localStorage.getItem("cart"));
+            const indexExistInCart = cart.findIndex(item => item.TourId == TourId);
+            if(indexExistInCart==-1){
+                cart.push({
+                    TourId: TourId,
+                    quantity: quantity
+                })
+            }else{
+                cart[indexExistInCart].quantity = cart[indexExistInCart].quantity + quantity
+            }
+            localStorage.setItem("cart",JSON.stringify(cart));
+        }
+    })
+}
+//end cart
