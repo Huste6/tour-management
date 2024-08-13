3fba9de9ecadfda88dc965cf65d03d635bd152f8
Huste6 <quannghihai1@gmail.com>
2024-08-13 09:22:13 +0700

    Làm slider

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,19 @@
-console.log("ok")
+// slider tour detail
+var imagesThumb = new Swiper(".imagesThumb", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+});
+var imagesMain = new Swiper(".imagesMain", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: imagesThumb,
+    },
+});
+// end slider tour detail
