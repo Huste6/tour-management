9eff3f90c6264add1979011d4abeda01fddd5ee4
Huste6 <quannghihai1@gmail.com>
2024-08-14 15:56:13 +0700

    upload nh anh

--- routes/admin/tour.route.ts
+++ routes/admin/tour.route.ts
@@ -1,12 +1,22 @@
 import { Router } from "express";
+import multer from "multer"
 import * as controller from "../../controllers/admin/tour.controller"
+import { uploadFields } from "../../helper/uploadToCloudinary";
 
 const router:Router = Router();
+const upload = multer();
 
 router.get("/",controller.index)
 
 router.get("/create",controller.create)
 
-router.post("/create",controller.createPost)
+router.post(
+    "/create",
+    upload.fields([
+        {name: 'images', maxCount: 10}
+    ]),
+    uploadFields,
+    controller.createPost
+)
 
 export const toursRoutes:Router = router;
